ff21c538def4ba22c411dfe92238c2d6e96c1f98
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-05-02 03:12:21 +0900

    proof of concept

--- src/main.zig
+++ src/main.zig
@@ -23,15 +23,16 @@ pub fn main() !void {
     try stream.sendHeader(alloc,hdr,true);    
     const request = h2client.getSendBuffer();
     defer request.deinit();
-    var netStream = try std.net.tcpConnectToHost(alloc,"localhost",443);
+    var netStream = try std.net.tcpConnectToHost(alloc,"shiguredo.jp",443);
     var bundle =  std.crypto.Certificate.Bundle{};
-    try bundle.addCertsFromFilePath(alloc,std.fs.cwd(),"test/cert/root_ca.crt");
+    try bundle.addCertsFromFilePath(alloc,std.fs.cwd(),"cacert.pem");
     const alpn :[3]u8 = "\x02h2".*;
-    var tlsClient = try TLSClient.init(netStream,bundle,"localhost",alpn);
+    var tlsClient = try TLSClient.init(netStream,bundle,"shiguredo.jp",alpn);
     try tlsClient.writeAll(&netStream,request.readableSlice(0));
     var peerHeader :?hpack.Header = null;
     defer if(peerHeader) |*d| d.deinit();
-
+    const  out = try std.fs.cwd().createFile("test.html",.{});
+    defer out.close();
     while(true) {
         const request2 = h2client.getSendBuffer();
         defer request2.deinit();
@@ -40,6 +41,9 @@ pub fn main() !void {
                 std.debug.print("writeAll error: {}\n",.{e});
                 break;
             };
+            if(h2client.goawayCode) |_| {
+                break;
+            }
         }
         var buf :[4096]u8 = undefined;
         const len = try tlsClient.read(&netStream,buf[0..]);
@@ -47,6 +51,8 @@ pub fn main() !void {
         if(peerHeader == null) {
             if(try stream.readHeader()) |header| {
                 peerHeader = header.header;
+                const stdout = std.io.getStdOut().writer();
+                try hpack.printHeader(stdout,peerHeader.?);
             }
             else {
                 continue;
@@ -55,13 +61,14 @@ pub fn main() !void {
         while(true) {
             const len2 = stream.recvData(buf[0..]);
             if(len2 == null) {
-                try h2client.sendGoaway(0,stream.id,null);
+                try h2client.sendGoaway(0,stream.id,null);                
                 break;
             }
             if(len2.? == 0) {
-                continue;
+                break;
             }
-            std.debug.print("{s}\n",.{buf[0..len2.?]});
+            std.debug.print("{s}",.{buf[0..len2.?]});
+            try out.writeAll(buf[0..len2.?]);
         }
     }
 }
